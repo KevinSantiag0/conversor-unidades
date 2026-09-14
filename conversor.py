8463b80ec4f36321d4294a3d83e5da60943e7846
KevinSantiag0 <kevin.santiago.gonzalez.2044@gmail.com>
2026-09-13 23:21:22 -0600

    Agrega menú principal y ciclo de repetición

--- conversor.py
+++ conversor.py
@@ -1,5 +1,33 @@
-print("*** Unit converter ***")
+def celsius_to_fahrenheit(c):
+    return c * 9/5 + 32
 
-temperature = float(input("Temperature - °C: "))
-fahrenheit = temperature * 9/5 + 32
-print(f"Temperature in fahrenheit: {fahrenheit:.2f}")
+
+def fahrenheit_to_celsius(f):
+    return (f-32)*5/9
+
+
+def show_menu():
+    print("=== Unit converter ===")
+    print("1)Celsius → Fahrenheit")
+    print("2)Fahrenheit → Celsius")
+    print("0)Exit")
+
+
+while True:
+
+    show_menu()
+    option = input("Choose an option: ")
+
+    if option == "1":
+        temperature_celsius = float(input("Temperature - °C: "))
+        fahrenheit = celsius_to_fahrenheit(temperature_celsius)
+        print(f"Temperature in fahrenheit: {fahrenheit:.2f}")
+    elif option == "2":
+        temperature_fahrenheit = float(input("Temperature - °F: "))
+        celsius = fahrenheit_to_celsius(temperature_fahrenheit)
+        print(f"Temperature in celsius: {celsius:.2f}")
+    elif option == "0":
+        print("Bye :)")
+        break
+    else:
+        print("Invalid option")
